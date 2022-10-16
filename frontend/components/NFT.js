77e5c6eab03775dbca284ec005fe59419e2ded88
Colin Steidtmann <colinsteidtmann@berkeley.edu>
2022-10-16 15:32:25 -0700

    final

--- frontend/components/NFT.js
+++ frontend/components/NFT.js
@@ -27,8 +27,8 @@ export default function NFT({ data }) {
     return (
 
         <div className="rounded-lg shadow-lg bg-white max-w-sm w-full">
-            <a href="#!">
-                <img className="rounded-t-lg mx-auto" src={data.imageUrl} alt="" />
+            <a href="_blank" rel="noreferrer">
+                <img rel="noreferrer" className="rounded-t-lg mx-auto" src={data.imageUrl} alt="" />
             </a>
             <div className="p-6">
                 <h5 className="text-gray-900 text-xl font-medium mb-2">{data.name}</h5>
