3f267c76751fd699187a30860be0c52edbf10aba
Colin Steidtmann <colinsteidtmann@berkeley.edu>
2022-10-16 14:37:22 -0700

    moved file

--- frontend/lib/contract.js
+++ frontend/lib/contract.js
@@ -1,3 +1,3 @@
-import ABI from "../../contracts/out/NFT.sol/NFT.json";
+import ABI from "./NFT.json";
 export const NFT_ADDRESS = "0xb02f10543f4069ddF2D656B5D3098A642Bc28071";
 export const NFT_ABI = ABI.abi;
