5c7cfa10d80a143fbc6c05b93910136731d45689
Colin Steidtmann <colinsteidtmann@berkeley.edu>
2022-10-15 20:20:57 -0700

    mint and display 2

--- contracts/src/NFT.sol
+++ contracts/src/NFT.sol
@@ -1,40 +1,92 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.9;
 
-import "openzeppelin-contracts/token/ERC721/ERC721.sol";
 import "openzeppelin-contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "openzeppelin-contracts/access/Ownable.sol";
 import "openzeppelin-contracts/utils/Counters.sol";
+import "openzeppelin-contracts/utils/Strings.sol";
+import "openzeppelin-contracts/utils/Base64.sol";
+import "openzeppelin-contracts/security/ReentrancyGuard.sol";
 
-contract NFT is ERC721, ERC721URIStorage, Ownable {
+contract NFT is ERC721URIStorage, ReentrancyGuard {
+    using Strings for uint256;
     using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
-    Counters.Counter private _tokenIdCounter;
+    constructor() ERC721("Lit NFT", "LITNFT") {}
 
-    constructor() ERC721("MyToken", "MTK") {}
+    mapping(uint256 => nft) private tokenIdToNft;
 
-    function safeMint(address to, string memory uri) public onlyOwner {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
+    struct nft {
+        string name;
+        string imageUrl;
+        string encryptedDescription;
+        string encryptedSymmetricKey;
     }
 
-    // The following functions are overrides required by Solidity.
-
-    function _burn(uint256 tokenId)
-        internal
-        override(ERC721, ERC721URIStorage)
-    {
-        super._burn(tokenId);
+    function getTokenURI(
+        string memory name,
+        string memory imageUrl,
+        string memory encryptedDescription,
+        string memory encryptedSymmetricKey
+    ) private pure returns (string memory) {
+        bytes memory dataURI = abi.encodePacked(
+            "{",
+            '"name": "',
+            name,
+            '",',
+            '"image": "',
+            imageUrl,
+            '",',
+            '"description": "',
+            encryptedDescription,
+            '",',
+            '"symmetricKey": "',
+            encryptedSymmetricKey,
+            '"',
+            "}"
+        );
+        return
+            string(
+                abi.encodePacked(
+                    "data:application/json;base64,",
+                    Base64.encode(dataURI)
+                )
+            );
     }
 
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
+    function mintLitNft(
+        string memory name,
+        string memory imageUrl,
+        string memory encryptedDescription,
+        string memory encryptedSymmetricKey
+    ) public nonReentrant {
+        _tokenIds.increment();
+        uint256 newNftTokenId = _tokenIds.current();
+        _safeMint(msg.sender, newNftTokenId);
+        _setTokenURI(
+            newNftTokenId,
+            getTokenURI(
+                name,
+                imageUrl,
+                encryptedDescription,
+                encryptedSymmetricKey
+            )
+        );
+        tokenIdToNft[newNftTokenId] = nft(
+            name,
+            imageUrl,
+            encryptedDescription,
+            encryptedSymmetricKey
+        );
+    }
+
+    function fetchNfts() public view returns (nft[] memory) {
+        nft[] memory nfts = new nft[](_tokenIds.current());
+        for (uint256 idx = 1; idx < _tokenIds.current() + 1; idx++) {
+            nft memory currNft = tokenIdToNft[idx];
+            nfts[idx - 1] = currNft;
+        }
+
+        return nfts;
     }
 }
